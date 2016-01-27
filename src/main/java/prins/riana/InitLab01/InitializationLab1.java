4f3078d3846db4e39464060f796ae87f5987932d
Riana Prins <rianaprins@Zipcoder07s-MacBook-Pro.local>
2016-01-27 18:55:38 -0500

    created BoringThing

---
+++ src/main/java/prins/riana/InitLab01/InitializationLab1.java
@@ -0,0 +1,25 @@
+package prins.riana.InitLab01;
+
+/**
+ * Created by rianaprins on 1/27/16.
+ */
+public class InitializationLab1 {
+
+    public static void main (String[] args) {
+        BoringThing boring1 = new BoringThing();
+        BoringThing boring2 = new BoringThing();
+        BoringThing boring3 = new BoringThing();
+        BoringThing boring4 = new BoringThing();
+        BoringThing boring5 = new BoringThing();
+
+        ColorfulThing color1 = new ColorfulThing("red");
+        ColorfulThing color2 = new ColorfulThing("red");
+        ColorfulThing color3 = new ColorfulThing("red");
+        ColorfulThing color4 = new ColorfulThing("red");
+        ColorfulThing color5 = new ColorfulThing("red");
+
+        //System.out.println()
+
+
+    }
+}
