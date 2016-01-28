097c9639d2d2989a7099b54f6f116a0366b783c8
Riana Prins <rianaprins@Zipcoder07s-MacBook-Pro.local>
2016-01-27 22:43:02 -0500

    final commit

--- src/main/java/prins/riana/InitLab01/ColorfulThing.java
+++ src/main/java/prins/riana/InitLab01/ColorfulThing.java
@@ -1,14 +1,13 @@
 package prins.riana.InitLab01;
 
-/**
- * Created by rianaprins on 1/27/16.
- */
+
 public class ColorfulThing {
     String color;
 
 
     public ColorfulThing(String color)  {
-       this.color = color;
+
+        this.color = color;
     }
 
 
